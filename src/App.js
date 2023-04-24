706d5bb16aca2dceb588ee011c04f971beed2ee4
citec-47 <mauricendonyi40@gmail.com>
2023-04-24 01:29:26 -0700

    React Up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable linebreak-style */
+import './App.css';
+
+function App() {
+// eslint-disable-next-line indent
+return (
+<h1>This is my First React Application</h1>);
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/order */
+/* eslint-disable linebreak-style */
+import React from 'react';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+
+reportWebVitals();
