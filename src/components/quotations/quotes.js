9e017629d235f2534c14997b4947707ce81b404d
citec-47 <mauricendonyi40@gmail.com>
2023-04-26 03:59:23 -0700

    adding quotes

--- src/components/quotations/quotes.js
+++ src/components/quotations/quotes.js
@@ -12,7 +12,7 @@ const MyQuotation = () => {
     const fetchQuotes = async () => {
       try {
         const response = await fetch(
-          'https://api.api-ninjas.com/v1/quotes?category=computers',
+          'https://api.api-ninjas.com/v1/cryptosymbols',
           {
             headers: {
               'X-Api-Key': 'bIjYCDyNgX1RI/dP1mdaMQ==JiMou8R3AeWdO2LE',
